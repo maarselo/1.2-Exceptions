0e7063aeb72d458c912f8913c28390903db8d861
maarselo <marce@decaos.com>
2025-07-03 13:27:43 +0200

    Add new files

--- src/Level1/model/Sale.java
+++ src/Level1/model/Sale.java
@@ -1,4 +1,25 @@
-package Level1;
+package Level1.model;
+
+import Level1.exception.VoidSaleException;
+
+import java.util.ArrayList;
 
 public class Sale {
+    private ArrayList<Product> products = null;
+    private double totalPrice;
+
+    public Sale() {
+        this.products = new ArrayList<>();
+        totalPrice = 0;
+    }
+
+    public void calculateTotal() throws VoidSaleException {
+        if (this.products.isEmpty())
+            throw new VoidSaleException("For do a sale, first you need to add products.");
+        for (Product p : products)
+            this.totalPrice += p.getPrice();
+        System.out.println("The total price is " +  this.totalPrice);
+    }
+
+    public ArrayList<Product> getProducts() {return products;}
 }
--- src/Level1/view/App.java
+++ src/Level1/view/App.java
@@ -1,4 +1,26 @@
 package Level1.view;
+import Level1.exception.VoidSaleException;
+import Level1.model.*;
 
 public class App {
+    public void run() {
+        Sale sale = new Sale();
+        try {
+            sale.calculateTotal();
+        } catch (VoidSaleException e) {
+            System.out.println(e.getMessage()); }
+        sale.getProducts().add(new Product("Spoon", 2.5));
+        sale.getProducts().add(new Product("Knife", 3.10));
+        sale.getProducts().add(new Product("Fork", 2.0));
+        sale.getProducts().add(new Product("Spatula", 5.0));
+        try {
+            sale.calculateTotal();
+        } catch (VoidSaleException e) {
+            System.out.println(e.getMessage());}
+        try {
+            Product p = sale.getProducts().get(4);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
