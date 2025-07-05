413de43c3156e56899e86ba28151a72e64ff82cc
maarselo <marce@decaos.com>
2025-07-05 19:40:58 +0200

    Rename the init of the app

--- src/Level2/view/App.java
+++ src/Level2/view/App.java
@@ -1,4 +1,16 @@
 package Level2.view;
 
+import Level2.utils.Entrada;
+
 public class App {
+    public static void run() {
+        byte numberByte = Entrada.readByte("How old are you?");
+        int numberInt = Entrada.readInt("How many brothers you have?");
+        float numberFloat = Entrada.readFloat("How much yor phone?");
+        double numberDouble = Entrada.readDouble("What was your average in school?");
+
+        char c = Entrada.readChar("Put a letter:");
+        String input = Entrada.readString("Put a phrase without digits:");
+        boolean response = Entrada.readYesOrNo("You are a good person? (y / n)");
+    }
 }
