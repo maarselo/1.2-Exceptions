4e0e4f68e38e647c02f6098e1f765dcf2db5f2cc
maarselo <marce@decaos.com>
2025-07-05 19:42:13 +0200

    Add the functions to check if it needs a exception

--- src/Level2/utils/Entrada.java
+++ src/Level2/utils/Entrada.java
@@ -1,7 +1,9 @@
-package Level2.model;
+package Level2.utils;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.InputMismatchException;
+import Level2.exceptions.*;
+import Level2.utils.*;
 
 public class Entrada {
     private static Scanner keyboard = new Scanner(System.in);
@@ -11,9 +13,11 @@ public class Entrada {
         while (true) {
             try {
                 System.out.println(message);
-                number = keyboard.nextByte(); break;
+                number = keyboard.nextByte();
+                keyboard.nextLine();break;
             } catch (InputMismatchException e) {
                 System.out.println(e.getMessage());
+                keyboard.nextLine();
             }
         }
         return (number);
@@ -23,9 +27,11 @@ public class Entrada {
         while (true) {
             try {
                 System.out.println(message);
-                number = keyboard.nextInt(); break;
+                number = keyboard.nextInt();
+                keyboard.nextLine(); break;
             } catch (InputMismatchException e) {
                 System.out.println(e.getMessage());
+                keyboard.nextLine();
             }
         }
         return (number);
@@ -35,9 +41,11 @@ public class Entrada {
         while (true) {
             try {
                 System.out.println(message);
-                number = keyboard.nextFloat(); break;
+                number = keyboard.nextFloat();
+                keyboard.nextLine(); break;
             } catch (InputMismatchException e) {
                 System.out.println(e.getMessage());
+                keyboard.nextLine();
             }
         }
         return (number);
@@ -47,12 +55,63 @@ public class Entrada {
         while (true) {
             try {
                 System.out.println(message);
-                number = keyboard.nextDouble(); break;
-            } catch (InputMismatchException e) {
+                number = keyboard.nextDouble();
+                keyboard.nextLine(); break;
+            } catch (InputMismatchException e ) {
                 System.out.println(e.getMessage());
+                keyboard.nextLine();
             }
         }
         return (number);
+    }
+
+
+    public static char readChar(String message) {
+        char    c;
+        while (true) {
+            try {
+                System.out.println(message);
+                c = keyboard.nextLine().charAt(0);
+                Utils.checkChar(c);
+                break;
+            } catch (InputHaveMetaChars e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return (c);
+    }
+    public static String readString(String message) {
+        String    input;
+        while (true) {
+            try {
+                System.out.println(message);
+                input = keyboard.nextLine();
+                Utils.checkInputIfIsEmpty(input);
+                Utils.checkStringDigits(input);
+                break;
+            } catch (InputHaveDigits | InputEmpty e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return (input);
+    }
+
+    public static Boolean readYesOrNo(String message) {
+        String    input;
+        while (true) {
+            try {
+                System.out.println(message);
+                input = keyboard.nextLine();
+                Utils.checkInputIfIsEmpty(input);
+                Utils.checkStringResponse(input);
+                if (input.equals("s"))
+                    return (true);
+                else
+                    return (false);
+            } catch (InputErrorResponse | InputEmpty e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
 
--- src/Level2/utils/Utils.java
+++ src/Level2/utils/Utils.java
@@ -1,4 +1,32 @@
 package Level2.utils;
 
+import Level2.exceptions.InputEmpty;
+import Level2.exceptions.InputErrorResponse;
+import Level2.exceptions.InputHaveDigits;
+import Level2.exceptions.InputHaveMetaChars;
+
 public class Utils {
+    public static void checkInputIfIsEmpty(String input) throws InputEmpty {
+        if (input.isEmpty())
+            throw new InputEmpty("The input cannot be empty.");
+    }
+
+    public static void checkChar(char c) throws InputHaveMetaChars {
+        String metaCharacters = "@#!$%^*-():;?/.,~+";
+        if (metaCharacters.indexOf(c) != -1)
+            throw new InputHaveMetaChars("Cannot be a metacharacter.");
+        return ;
+    }
+    public static void checkStringDigits(String input) throws InputHaveDigits {
+        String digits = "0123456789";
+        for (char c : digits.toCharArray())
+            if (input.indexOf(c) != -1)
+                throw new InputHaveDigits("The string must no have digits.");
+        return ;
+    }
+    public static void checkStringResponse(String input) throws InputErrorResponse {
+        if (input.length() != 1 || input.contains("s") || input.contains("n"))
+            throw new InputErrorResponse("Must be a char like <y> or <n>.");
+        return;
+    }
 }
